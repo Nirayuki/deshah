9f4b970ef96bb1a98e0a638a9748da9b9b14fef8
Aron felippe kerkhoven <aron@dotse.com.br>
2021-02-28 18:32:28 -0300

    rotas no header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Home from '../src/Pages/Home/index';
+import Header from '../src/components/Header';
+import Routes from './routes';
+import {Grid} from '@material-ui/core';
 
 function App() {
   return (
-    <Home>
-      
-    </Home>
+      <Grid>
+        <Header/>
+        <Routes/>
+      </Grid>
+    
   );
 }
 
