3292acdb0728fbd037a35b4a4752fa086bd5c040
Henrique Oliveira <henrique.gabriel.de.oliveira43@gmail.com>
2021-03-01 18:22:02 -0300

    atualização 1

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Home from '../src/Pages/Home/index';
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
+import { Route, Switch} from 'react-router-dom';
 
 
 const Routes = () => {
     return(
-        <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Home}/>
             </Switch>
-        </BrowserRouter>
     )
 }
 
