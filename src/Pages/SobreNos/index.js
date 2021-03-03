dda0e28406e46f673244b072f9a7d4947e956b47
Henrique Oliveira <henrique.gabriel.de.oliveira43@gmail.com>
2021-03-02 22:08:33 -0300

    pequena introdução por hora

--- src/Pages/SobreNos/index.js
+++ src/Pages/SobreNos/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 export default function SobreNos(){
     return(
-        <div>
-            <h1>
-                vou tomar banho
-            </h1>
-        </div>
+        <head>
+
+            <h3>Sobre Nos</h3>
+
+        </head>~
+
+        <body>
+                <p> Fazemos parte do curso de Engenharia de Software da Universiade, FAG, Faculdade </p>
+                <p>assis gurgacz, e estamos desenvolvendo esse jogo como forma de projeto para o nosso</p>
+                <p>  desenvolvimento como futoro programadores, esse jogo, apesar de simples, um grande </p>
+                <p>significado para nos, pois e o nosso primeiro projeto.</p>
+
+        </body>
     )
 }
