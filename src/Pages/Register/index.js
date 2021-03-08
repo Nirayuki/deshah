03261a2b078ddc3a81391cb909373a6cfc91566b
Aron felippe kerkhoven <aron@dotse.com.br>
2021-03-08 12:40:19 -0300

    Deshah Update

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import '../../styles/register.css';
 
 
 export default function Register(){
     return(
-        <div>
-            Oi
-        </div>
+        <>  
+            <div className="register_container">
+            <center>
+                <form autoComplete="off" id="register">
+                    <input id="email" type="email" name="email" placeholder="Email" onFocus/>
+                    <input id="name" name="name" type="text" placeholder="Username" maxLength="20"/>
+                    <input id="password" type="password" placeholder="Password" maxLength="20"/>
+                    <button type="submit">Register</button>
+                </form>
+                </center>
+            </div>
+        </>
     )
 }
