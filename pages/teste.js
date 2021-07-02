c11ebe86acc35e2535c62644d4e453e79b27245b
Aron Felippe Kerkhoven <43013959+Nirayuki@users.noreply.github.com>
2021-07-02 14:33:48 -0300

    Background att

---
+++ pages/teste.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../styles/Teste.module.css'
+
+
+export default function Teste(){
+    return(
+        <div className={styles.background_lite}>
+            Oi
+        </div>
+    )
+}
