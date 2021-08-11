233c39212ecd09aff1a85abf2e976a0875955faa
Aron Felippe Kerkhoven <aronkerk8@gmail.com>
2021-08-11 19:33:00 -0300

    Update NavBar.js

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -13,7 +13,7 @@ export default function NavBar() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/about">About</Nav.Link>
+                        {/* <Nav.Link href="/about">About</Nav.Link> */}
                         <Nav.Link href="#link">Link</Nav.Link>
                         {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
